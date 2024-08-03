3f9bd63d60ae40a9c6f2a5de24b757806b186f76
hasanboy <hasanboyhakimov95@gmail.com>
2024-08-03 10:07:59 +0500

    initial commit

--- src/main/java/org/example/springcryptoautomaticalyjava/SpringCryptoAutomaticalyJavaApplication.java
+++ src/main/java/org/example/springcryptoautomaticalyjava/SpringCryptoAutomaticalyJavaApplication.java
@@ -2,12 +2,14 @@ package org.example.springcryptoautomaticalyjava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringCryptoAutomaticalyJavaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringCryptoAutomaticalyJavaApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(SpringCryptoAutomaticalyJavaApplication.class, args);
+        SpringApplication.exit(run);
     }
 
 }
---
+++ src/main/java/org/example/springcryptoautomaticalyjava/repository/ReportHistoryJpaRepository.java
@@ -0,0 +1,8 @@
+package org.example.springcryptoautomaticalyjava.repository;
+
+import org.example.springcryptoautomaticalyjava.entity.ReportHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReportHistoryJpaRepository extends JpaRepository<ReportHistory, Integer> {
+
+}
